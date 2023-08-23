8c849c62d8ab32848a9e9f270eb90c92d8262c6c
filippo199600 <pippotartaglia@gmail.com>
2023-08-23 15:15:07 +0200

    add time of now and tomorrow

--- js/main.js
+++ js/main.js
@@ -5,3 +5,10 @@ const oneHour = oneMinute * 60;
 const oneDay = oneHour * 24;
 
 console.log(oneDay);
+
+// cotante che definisca il tempo di adesso
+const nowDate = new Date();
+
+// costante che mi definisca il tempo di domani
+const tomorrowDate = new Date(2023, 7, 24, 9, 30, 0, 0);
+console.log(tomorrowDate);
