990b57de58b800b98b652c1c3161d3172de9216e
filippo199600 <pippotartaglia@gmail.com>
2023-08-23 16:52:18 +0200

    fix refresh error

--- js/main.js
+++ js/main.js
@@ -12,19 +12,19 @@ const oneDay = oneHour * 24;
 
 console.log(oneDay);
 
-// cotante che definisca il tempo di adesso
-const nowDate = new Date();
-
-// costante che mi definisca il tempo di domani
-const tomorrowDate = new Date(2023, 7, 24, 9, 30, 0, 0);
-console.log(tomorrowDate);
-
-// definisco il tempo rimanente
-const missingTime = tomorrowDate - nowDate;
-console.log(missingTime);
-
 //creo una funzione orologio
 function getTimeWatch() {
+  // cotante che definisca il tempo di adesso
+  const nowDate = new Date();
+
+  // costante che mi definisca il tempo di domani
+  const tomorrowDate = new Date(2023, 7, 24, 9, 30, 0, 0);
+  // console.log(tomorrowDate);
+
+  // definisco il tempo rimanente
+  const missingTime = tomorrowDate - nowDate;
+  console.log(missingTime);
+
   // converto il calcolo del missingTime
   // giorni
   const missingDay = Math.floor(missingTime / oneDay);
