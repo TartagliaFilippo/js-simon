42b17b69540d5c72aa43a9ffb9466add9460fcd4
filippo199600 <pippotartaglia@gmail.com>
2023-08-23 14:58:54 +0200

    add const time

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// le mie costanti globali di tempo
+const oneSecond = 1000;
+const oneMinute = oneSecond * 60;
+const oneHour = oneMinute * 60;
+const oneDay = oneHour * 24;
+
+console.log(oneDay);
